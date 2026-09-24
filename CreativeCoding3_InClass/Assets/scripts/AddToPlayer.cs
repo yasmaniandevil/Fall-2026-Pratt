11a46447dbdf448a0082ae9fac30108630e2a239
yasmaniandevil <44387512+yasmaniandevil@users.noreply.github.com>
2026-09-23 20:57:30 -0400

    finished score and object mover

--- CreativeCoding3_InClass/Assets/scripts/AddToPlayer.cs
+++ CreativeCoding3_InClass/Assets/scripts/AddToPlayer.cs
@@ -2,15 +2,54 @@ using UnityEngine;
 
 public class AddToPlayer : MonoBehaviour
 {
+    public GameObject uiHolder;
+    //we need a reference to the score script so that we can use AddScore Function
+    //so Score below is OUR SCRIPT
+    private Score scoreScript;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //so this means UI holder is the GAMEOBJECT that has score script attached to
+        //we first need to say the gameobject then GET COMPNENT
+        scoreScript = uiHolder.GetComponent<Score>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //a trigger you can go through
+    //if its a coin you want to pass through the coin you dont want it to block you from moving forward
+    private void OnTriggerEnter(Collider other)
+    {
+        //other is the thing that we are hitting(which will be our cubeprefab)
+        if (other.CompareTag("CubePrefab"))
+        {
+            //Debug.Log("hit obstacle");
+            //this is our score script, we are accessing our function called add scored
+            scoreScript.AddScore(1);
+            //after all of that is done then destroy the cubePrefab
+            Destroy(other.gameObject);
+        }
+    }
+
+    //if we collide with a gameobject that has the tag "spherePrefab"
+    //we PHYSICALLY collide, physically hit each other
+    //then subtract from my score
+    //Collision collision is like what we do at the top. Collision is the type of var we want and collision is the name of it, it can be named anything!!!
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("made collision");
+
+        if (collision.gameObject.CompareTag("SpherePrefab"))
+        {
+            Debug.Log("Detected sphere");
+            Debug.Log("hit sphere");
+            scoreScript.AddScore(-1);
+            Destroy(collision.gameObject);
+        }
     }
 }
--- CreativeCoding3_InClass/Assets/scripts/ObjectMover.cs
+++ CreativeCoding3_InClass/Assets/scripts/ObjectMover.cs
@@ -2,15 +2,43 @@ using UnityEngine;
 
 public class ObjectMover : MonoBehaviour
 {
+    private Vector3 startPos; //this vector 3 is empty it is 0, 0, 0
+    public string functionName;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //here we are saying wherever the obj this script is attached to take its position
+        //and give it to StartPos
+        startPos = transform.position;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        SendMessage(functionName);
+    }
+
+    void RotateInPlace()
+    {
+        //just rotates on the y
+        transform.Rotate(0, 90 * Time.deltaTime, 0, Space.Self);
+        
+    }
+
+    void PingPong()
+    {
+        //moveDir IS OUR VARIABLE!!!
+        //its like writing it at the top
+        //but here we are immeditely giving it something
+        //and we dont write it at the top because we only need it here
+        Vector3 moveDir = Vector3.right;
+        float moveDistance = 2f;
+        float moveSpeed = .5f;
+
+        float t = Mathf.PingPong(Time.time * moveSpeed, 1f);
+        Vector3 offset = moveDir.normalized * (t * moveDistance);
+        transform.position = startPos + offset;
         
     }
 }
